dbfd1527eea444246213d18958716845954113b1
Lorenzo Cano <lcano@unizar.es>
2022-12-13 18:42:37 +0100

    "deleted irrelevant files"

--- perlin_noise_trials.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-from perlin_noise import PerlinNoise
-noise = PerlinNoise(octaves=100, seed=1)
-xpix, ypix = 1000, 1000
-pic = [[noise([i/xpix, j/ypix]) for j in range(xpix)] for i in range(ypix)]
-
-plt.imshow(pic, cmap='gray')
-plt.show()
--- pruebas.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-print(plt.gca())
-
-fig = plt.figure()
-axis = fig.add_subfigure()
-
-print(plt.gca())
