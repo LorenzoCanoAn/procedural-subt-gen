41666253020b3470982d993dedac12f43332f113
Lorenzo <lcano@unizar.es>
2023-03-11 01:20:47 +0100

    Added the classes to store the parameters of the tunnel generation

--- src/subt_proc_gen/tunnel_2.py
+++ src/subt_proc_gen/tunnel_2.py
@@ -2,8 +2,71 @@ from subt_proc_gen.graph import Node, Edge, Graph
 from subt_proc_gen.geometry import Point3D, Vector3D, Spline3D
 
 
+class ConnectorTunnelGenerationParams:
+    def __init__(
+        self,
+        first_segment_direction=None,
+        last_segment_direction=None,
+        min_segment_length=None,
+        node_position_horizontal_noise=None,
+        node_position_vertical_noise=None,
+    ):
+        assert (
+            not min_segment_length is None
+            and not node_position_horizontal_noise is None
+            and not node_position_vertical_noise is None
+        )
+        self.first_segment_direction = first_segment_direction
+        self.last_segment_direction = last_segment_direction
+        self.min_segment_length = min_segment_length
+        self.node_position_horizontal_noise = node_position_horizontal_noise
+        self.node_position_vertical_noise = node_position_horizontal_noise
+
+
+class GrownTunnelGenerationParams:
+    def __init__(
+        self,
+        distance=None,
+        starting_direction=None,
+        horizontal_tendency=None,
+        vertical_tendency=None,
+        horizontal_noise=None,
+        vertical_noise=None,
+        segment_length=None,
+        segment_length_noise=None,
+    ):
+        assert (
+            not distance is None
+            and not starting_direction is None
+            and not horizontal_tendency is None
+            and not vertical_tendency is None
+            and not horizontal_noise is None
+            and not vertical_noise is None
+            and not segment_length is None
+            and not segment_length_noise is None
+        )
+        self.distance = distance
+        self.starting_direction = starting_direction
+        self.horizontal_tendency = horizontal_tendency
+        self.vertical_tendency = vertical_tendency
+        self.horizontal_noise = horizontal_noise
+        self.vertical_noise = vertical_noise
+        self.segment_length = segment_length
+        self.segment_length_noise = segment_length_noise
+
+
 class Tunnel:
     """A tunnel only contains nodes"""
+
+    @classmethod
+    def grown(cls, node: Node, params: GrownTunnelGenerationParams):
+        pass
+
+    @classmethod
+    def connector(
+        cls, i_node: Node, f_node: Node, params: ConnectorTunnelGenerationParams
+    ):
+        pass
 
     def __init__(self, nodes):
         if isinstance(nodes, list):
@@ -55,7 +118,7 @@ class TunnelNetwork(Graph):
         super().remove_node(node)
         del self._tunnels_of_node[node]
 
-    def add_tunnel(self, tunnel: Tunnel):
+    def _add_tunnel(self, tunnel: Tunnel):
         self._tunnels.add(tunnel)
         for node in tunnel:
             self.add_node(node)
@@ -63,7 +126,7 @@ class TunnelNetwork(Graph):
         for ni, nj in zip(tunnel[:-1], tunnel[1:]):
             self.connect(ni, nj)
 
-    def remove_tunnel(self, tunnel: Tunnel):
+    def _remove_tunnel(self, tunnel: Tunnel):
         for node in tunnel:
             self._tunnels_of_node[node].remove(tunnel)
             if len(self._tunnels_of_node[node] == 0):
