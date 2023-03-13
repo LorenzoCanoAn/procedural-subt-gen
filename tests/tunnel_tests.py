b95970beed3cce3bd1ae38607ce81a3e6b507f80
lcano@unizar.es <lcano@unizar.es>
2023-03-13 14:55:25 +0100

    added test for growing tunnel

--- tests/tunnel_tests.py
+++ tests/tunnel_tests.py
@@ -1,6 +1,6 @@
-from subt_proc_gen.tunnel import TunnelNetwork
+from subt_proc_gen.tunnel_2 import TunnelNetwork, Tunnel, GrownTunnelGenerationParams
 from subt_proc_gen.graph import Node
-from subt_proc_gen.display_functions import plot_nodes
+from subt_proc_gen.display_functions import plot_nodes, plot_edges, plot_xyz_axis
 import pyvista as pv
 
 
@@ -12,13 +12,30 @@ def test1():
             for z in values:
                 tunnel_network.add_node(Node((x, y, z)))
 
+
+def test2():
+    tunnel_network = TunnelNetwork()
+    first_node = Node((0, 0, 0))
+    tunnel_network.add_node(first_node)
+    tunnel_network.add_tunnel(
+        Tunnel.grown(
+            i_node=first_node, params=GrownTunnelGenerationParams.from_defaults()
+        )
+    )
+    params = GrownTunnelGenerationParams.random()
+    tunnel_network.add_tunnel(Tunnel.grown(i_node=first_node, params=params))
+    print(params)
+    tunnel_network._tunnels
     plotter = pv.Plotter()
     plot_nodes(plotter, tunnel_network.nodes)
+    plot_edges(plotter, tunnel_network.edges)
+    plot_xyz_axis(plotter)
     plotter.show()
 
 
 def main():
     test1()
+    test2()
 
 
 if __name__ == "__main__":
